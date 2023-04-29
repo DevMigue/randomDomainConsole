1d37db428c1160490b3f9e375d37797d8a5d12cd
DevMigue <127422296+DevMigue@users.noreply.github.com>
2023-04-29 06:56:48 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
-let who = ['The dog','My grandma','His turtle','My bird'];
-let action = ['ate','peed','crushed','broke'];
-let what = ['my homework', 'the keys', 'the car'];
-let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big', 'exuberant', 'fearless', 'vibrant', 'jovial'];
+let noun = ['jogger', 'racoon', 'giraffe', 'muffin', 'pickle', 'penguin'];
+let extension = ['.com', '.net', '.org', '.io', '.dev', '.info'];
 
-function randomExcuse() {
-  let whoIndex = Math.floor(Math.random() * who.length);
-  let actionIndex = Math.floor(Math.random() * action.length);
-  let whatIndex = Math.floor(Math.random() * what.length);
-  let whenIndex = Math.floor(Math.random() * when.length);
-
-  let excuse = `${who[whoIndex]} ${action[actionIndex]} ${what[whatIndex]} ${when[whenIndex]}`;
-  return excuse;
+function generateDomain() {
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < extension.length; l++) {
+          const domain = `${pronoun[i]}${adj[j]}${noun[k]}${extension[l]}`;
+          console.log(domain);
+        }
+      }
+    }
+  }
 }
 
-function changeExcuse() {
-    let excuseElement = document.getElementById("excuse");
-    excuseElement.innerHTML = randomExcuse();
-  }
+generateDomain();
